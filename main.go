a6b9bbe8a05a0ef846bada8fc3c19cdd11f070d7
vinayaka <me.vinayakakv@gmail.com>
2018-10-23 20:01:43 +0530

    Added test code for Peer Discovery

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	peer "./src/peer-to-peer"
 	//"fmt"
+	"fmt"
 )
 
 func main() {
 	p := peer.CreatePeer(8090)
-	p.AddPeer("0.0.0.0:9000")
+	//p.AddPeer("0.0.0.0:9000")
+	p.AddHandler("PING", peer.HandlePING)
+	p.Start()
+	addr := ""
+	fmt.Printf("Enter the adress of the client to connect to\n")
+	fmt.Scanf("%s",addr)
+	p.AddPeer(addr)
 }
